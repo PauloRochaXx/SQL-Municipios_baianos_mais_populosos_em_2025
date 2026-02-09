f8ebfb59f67efb627ecdd6d62baa7fe8dee7b859
PauloRochaXx <paulocoba@hotmail.com>
2026-02-09 14:50:10 -0300

    Update 03_tratamento.sql
    Adição de comentários na Query3

--- scripts/03_tratamento.sql
+++ scripts/03_tratamento.sql
@@ -1,9 +1,15 @@
 --=================================================================================
+--Remover colunas desnecessárias e padronizar códigos de município entre tabelas
+--=================================================================================
 
+-- Remove coluna desnecessária da tabela de códigos de municípios
+-- A coluna 'column6' não é utilizada nas análises, e sua remoção deixa a tabela mais enxuta
 ALTER TABLE dbo.tabela_cod_mun
 	DROP COLUMN column6;
 
 
+-- Copia o código de município da tabela de códigos para a tabela de população
+-- Garante que os registros em POP2025_20260113 estejam alinhados com a tabela de referência
 UPDATE dbo.POP2025_20260113
 	SET cod_municipio = dbo.tabela_cod_mun.cod_municipio
 FROM dbo.POP2025_20260113
@@ -11,6 +17,8 @@ INNER JOIN dbo.tabela_cod_mun
 	ON dbo.POP2025_20260113.nome_municipio = dbo.tabela_cod_mun.nome_municipio;
 
 
+-- Copia o código de município da tabela de códigos para a tabela de territórios de identidade
+-- Garante que os registros em territorios_identidade_bahia estejam alinhados com a tabela de referência
 UPDATE dbo.territorios_identidade_bahia
 	SET cod_municipio = dbo.tabela_cod_mun.cod_municipio
 FROM dbo.territorios_identidade_bahia
